b9e9274de68c118019719fab4dd45330284653e9
adpl18 <adominguez18>
2024-06-07 19:38:15 -0400

    add cognito

--- src/Routes.js
+++ src/Routes.js
@@ -2,11 +2,13 @@ import React from "react";
 import { useRoutes } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/404";
+import Profile from "./pages/Profile";
 
 const ProjectRoutes = () => {
     let element = useRoutes([
         { path: "/", element: <Home /> },
-        { path: "/404", element: <NotFound />}
+        { path: "/404", element: <NotFound />},
+        { path: "/profile", element: <Profile />},
     ]);
 
     return element;
---
+++ src/auth/authService.js
@@ -0,0 +1,136 @@
+import { 
+  CognitoIdentityProviderClient, 
+  InitiateAuthCommand, 
+  SignUpCommand, 
+  ConfirmSignUpCommand, 
+  ForgotPasswordCommand, 
+  ConfirmForgotPasswordCommand,
+  GetUserCommand
+} from "@aws-sdk/client-cognito-identity-provider";
+
+// test1234code
+
+export const cognitoClient = new CognitoIdentityProviderClient({
+  region: process.env.REACT_APP_REGION_AWS,
+});
+
+export const signIn = async (username, password) => {
+  const params = {
+    AuthFlow: "USER_PASSWORD_AUTH",
+    ClientId: process.env.REACT_APP_CLIENT_ID_AWS,
+    AuthParameters: {
+      USERNAME: username,
+      PASSWORD: password,
+    },
+  };
+  try {
+    const command = new InitiateAuthCommand(params);
+    const { AuthenticationResult } = await cognitoClient.send(command);
+    if (AuthenticationResult) {
+      sessionStorage.setItem("idToken", AuthenticationResult.IdToken || '');
+      sessionStorage.setItem("accessToken", AuthenticationResult.AccessToken || '');
+      sessionStorage.setItem("refreshToken", AuthenticationResult.RefreshToken || '');
+      return AuthenticationResult;
+    }
+  } catch (error) {
+    console.error("Error signing in: ", error);
+    throw error;
+  }
+};
+
+export const signUp = async (email, password) => {
+  const params = {
+    ClientId: process.env.REACT_APP_CLIENT_ID_AWS,
+    Username: email,
+    Password: password,
+    UserAttributes: [
+      {
+        Name: "email",
+        Value: email,
+      },
+    ],
+  };
+  try {
+    const command = new SignUpCommand(params);
+    const response = await cognitoClient.send(command);
+    console.log("Sign up success: ", response);
+    return response;
+  } catch (error) {
+    console.error("Error signing up: ", error);
+    throw error;
+  }
+};
+
+export const confirmSignUp = async (username, code) => {
+  const params = {
+    ClientId: process.env.REACT_APP_CLIENT_ID_AWS,
+    Username: username,
+    ConfirmationCode: code,
+  };
+  try {
+    const command = new ConfirmSignUpCommand(params);
+    await cognitoClient.send(command);
+    console.log("User confirmed successfully");
+    return true;
+  } catch (error) {
+    console.error("Error confirming sign up: ", error);
+    throw error;
+  }
+};
+
+
+export const forgotPassword = async (email) => {
+  const params = {
+    ClientId: process.env.REACT_APP_CLIENT_ID_AWS,
+    Username: email,
+  };
+
+  try {
+    const command = new ForgotPasswordCommand(params);
+    await cognitoClient.send(command);
+  } catch (error) {
+    throw new Error(`Failed to initiate password reset: ${error.message}`);
+  }
+};
+
+export const resetPassword = async (email, code, password) => {
+  const params = {
+    ClientId: process.env.REACT_APP_CLIENT_ID_AWS,
+    Username: email,
+    ConfirmationCode: code,
+    Password: password,
+  };
+
+  try {
+    const command = new ConfirmForgotPasswordCommand(params);
+    await cognitoClient.send(command);
+  } catch (error) {
+    throw new Error(`Failed to initiate password reset: ${error.message}`);
+  }
+};
+
+export const getUserInfo = async (accessToken) => {
+  const params = {
+    AccessToken: accessToken,
+  };
+
+  try {
+    const command = new GetUserCommand(params);
+    const response = await cognitoClient.send(command);
+
+    const userAttributes = {};
+    response.UserAttributes.forEach(item => {
+        userAttributes[item.Name] = item.Value;
+    });
+    return userAttributes;
+  } catch (error) {
+    console.error('Error getting user info:', error);
+    throw error;
+  }
+};
+
+export const logout = () => {
+  sessionStorage.removeItem('idToken');
+  sessionStorage.removeItem('accessToken');
+  sessionStorage.removeItem('refreshToken');
+};
